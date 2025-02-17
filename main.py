b9351931e2d71dcbb93e292f382862fd44c8c114
theus.dev <matheusmnt13@outlook.com>
2025-02-17 20:14:32 -0300

    iniciando estudos de python

---
+++ main.py
@@ -0,0 +1,66 @@
+# print("Hello, World!") # print é o comando para exibir algo na tela
+
+# Váriaveis sao espaços na memória que guardam valores
+# Imput é o comando para receber algo do usuário
+
+# nome = input("Digite seu nome: ")
+# idade = int(input("Digite sua idade: "))
+# altura = float(input("Digite sua altura: "))
+
+# print(nome)
+# print(type(idade))
+# print(type(altura))
+
+
+# Operadores aritméticos
+
+# soma = 10 + 10
+# multiplicacao = 10 * 10
+# divisao = 10 / 10
+# potencia = 2 ** 10
+
+# print("soma", soma)
+# print("multiplicacao", multiplicacao)
+# print("divisao", divisao)
+# print("potencia", potencia)
+
+
+# Condicionais
+# idade = int(input("Digite sua idade: "))
+
+# if idade >= 18:
+#     print("Você pode beber")
+# else:
+#     print("Você não pode beber")
+
+
+
+# IF ELSE ELIF São estruturas de condição que permitem avaliar uma expressão e, de acordo com seu resultado, executar uma determinada ação.
+# salario = float(input("Digite seu salário: "))
+
+# if salario <= 3000:
+#   print("Dev Junior")
+# elif salario > 3000 and salario <= 6000:
+#   print("Dev Pleno")
+# elif salario > 6000 and salario <= 10000:
+#   print("Dev Senior")
+# else:
+#   print("Project Manager")    
+
+
+# Listas São coleções de elementos que podem ser de diferentes tipos, como inteiros, strings, floats, etc.
+
+lista_numeros = [1, 2, 3]
+
+lista_numeros[0] = 10
+
+print(lista_numeros[0])
+print(lista_numeros[1])
+print(lista_numeros[2])
+
+lista_vazia = []
+
+lista_vazia.append("Hello")
+lista_vazia.append("World")
+
+print(lista_vazia)
