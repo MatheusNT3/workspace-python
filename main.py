7b39af2677fe401da8053cb3469a2989cd9c1d8a
theus.dev <matheusmnt13@outlook.com>
2025-02-18 20:37:47 -0300

    estudo sobre loops

--- main.py
+++ main.py
@@ -50,17 +50,41 @@
 
 # Listas São coleções de elementos que podem ser de diferentes tipos, como inteiros, strings, floats, etc.
 
-lista_numeros = [1, 2, 3]
+# lista_numeros = [1, 2, 3]
 
-lista_numeros[0] = 10
+# lista_numeros[0] = 10
 
-print(lista_numeros[0])
-print(lista_numeros[1])
-print(lista_numeros[2])
+# print(lista_numeros[0])
+# print(lista_numeros[1])
+# print(lista_numeros[2])
 
-lista_vazia = []
+# lista_vazia = []
 
-lista_vazia.append("Hello")
-lista_vazia.append("World")
+# append serve para adicionar um elemento na lista
+# lista_vazia.append("Hello")
+# lista_vazia.append("World")
 
-print(lista_vazia)
+# print(lista_vazia)
+
+# numeros = [10, 9, 8, 7, 6, 5]
+
+# print("Total: ", len(numeros))
+# print("Menor número: ", min(numeros))
+# print("Maior número: ", max(numeros))
+
+
+# For é uma estrutura de repetição que permite executar um bloco de código várias vezes.
+notas = []
+
+for x in range(10):
+    codigo_aluno = input("RM: ")
+    nota = float(input("Nota: "))
+    resultado = [codigo_aluno, nota]
+    notas.append(resultado)
+
+print("Quantidade de notas", len(notas))  
+
+for n in notas:
+  codigo_aluno = n[0]
+  nota = n[1]
+  print("O RM: ", codigo_aluno, "Tirou a nota: ", nota)
