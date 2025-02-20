ac14e7a3f08c2e2253a3b34fb5ae8fce8fc13de3
theus.dev <matheusmnt13@outlook.com>
2025-02-19 21:31:51 -0300

    dia 3

--- main.py
+++ main.py
@@ -74,17 +74,35 @@
 
 
 # For é uma estrutura de repetição que permite executar um bloco de código várias vezes.
-notas = []
+# notas = []
 
-for x in range(10):
-    codigo_aluno = input("RM: ")
-    nota = float(input("Nota: "))
-    resultado = [codigo_aluno, nota]
-    notas.append(resultado)
+# for x in range(10):
+#     codigo_aluno = input("RM: ")
+#     nota = float(input("Nota: "))
+#     resultado = [codigo_aluno, nota]
+#     notas.append(resultado)
 
-print("Quantidade de notas", len(notas))  
+# print("Quantidade de notas", len(notas))  
 
-for n in notas:
-  codigo_aluno = n[0]
-  nota = n[1]
-  print("O RM: ", codigo_aluno, "Tirou a nota: ", nota)
+# for n in notas:
+#   codigo_aluno = n[0]
+#   nota = n[1]
+#   print("O RM: ", codigo_aluno, "Tirou a nota: ", nota)
+
+
+
+# While é uma estrutura de repetição que permite executar um bloco de código várias vezes.
+# notas = []
+# contador = 1
+
+# while contador <= 5:
+#   codigo_aluno = input("RM: ")  
+#   nota = float(input("Nota: "))
+#   resultado = [codigo_aluno, nota]
+#   notas.append(resultado)
+
+#   contador = contador + 1
+
+# print("Quantidade de notas", len(notas))
+
+# Dicionários São coleções de elementos que são indexados por chaves, ou seja, são pares de chave e valor.
