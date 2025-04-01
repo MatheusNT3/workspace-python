2c08672fd30bed94379b67a57fe6564996a02c44
theus.dev <matheusmnt13@outlook.com>
2025-04-01 17:09:45 -0300

    desafio 006 concluído

---
+++ curso-python-guanabara/exercicios/exec006/main.py
@@ -0,0 +1,4 @@
+# Desenlvolva um programa que leia um número inteiro e mostre na tela o seu dobro, triplo e raiz quadrada.
+
+n = int(input('Digite um número: '))
+print(f' o numero {n} tem o dobro {n*2}, o triplo {n*3} e a raíz quadrada {n**(1/2)}')
