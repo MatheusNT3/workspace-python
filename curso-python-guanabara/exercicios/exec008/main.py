f77d1598118e4fa5064002c9dd2e8c8d0fa93821
theus.dev <matheusmnt13@outlook.com>
2025-04-01 18:50:28 -0300

    desafio 008 concluído

---
+++ curso-python-guanabara/exercicios/exec008/main.py
@@ -0,0 +1,4 @@
+# crie um programa que transforme metros em centimetros e milimetros
+
+n = float(input('Digite um valor em metros: '))
+print(f'O valor de {n} em centimetros é {n * 100} e em milimetros é {n * 1000}')
